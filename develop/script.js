98871d479415f6bf3bb3d89518d513ea05bc2d88
Sydney Laub <syd.laub18@gmail.com>
2023-05-09 18:40:34 -0400

    added code to display the 5 day weather forecast in js

--- develop/script.js
+++ develop/script.js
@@ -8,10 +8,10 @@ $("#cardTodayDate").text(today.format('MMMM DD, YYYY'));
 
 
 // need to save the user search and store it to an array in local storage
-var cityHistory = [];
+var cityHistory = JSON.parse(localStorage.getItem("city")) || [];
 $('.btn-search').on("click", function(event){
     event.preventDefault();
-    weatherDisplay($(this))
+    // weatherDisplay($(this))
     city = $(this).parent(".btnParent").siblings(".btnTextVal").val().trim();
     if (city === "") {
         return;
@@ -21,9 +21,37 @@ $('.btn-search').on("click", function(event){
 
     // store in localStorage
     localStorage.setItem('city', JSON.stringify(cityHistory));
+    weatherDisplay($(this));
 
 })
 
+var cityHistoryEl = $('.cityHistory')
+function displayHistory() {
+    cityHistoryEl.empty();
+
+    for (let i = 0; i < cityHistory.length; i++){
+        var row = $('<row>');
+        var cityBtn = $('<button>').text(`${cityHistory[i]}`)
+
+        row.addClass('row cityBtnRow');
+        cityBtn.addClass('btn btn-outline-secondary cityBtn')
+        cityBtn.attr("type", "button");
+
+        cityHistoryEl.prepend(row);
+        row.append(cityBtn);
+    } if (!city) {
+        return; 
+    }
+
+    // function to make the city history buttons prompt a search
+    $('.cityBtn').on("click", function (event){
+        event.preventDefault();
+        console.log(event)
+        weatherDisplay($(event.target.innerText));
+    })
+};
+
+displayHistory()
 
 // need to turn the city's in search history into buttons
 
@@ -51,8 +79,8 @@ function weatherDisplay (event) {
         // current weather display code goes here
         $(".cardTodayCityName").text(response.name)
         var ul = $("<ul>")
+        
         // current temp display
-
         var liTemp = $("<li>")
         liTemp.text("Temperature: " + response.main.temp + "°F")
 
@@ -62,7 +90,7 @@ function weatherDisplay (event) {
 
         // current wind speed display
         var liWind = $("<li>")
-        liWind.text("Wind speed: " + response.wind.speed + "mph")
+        liWind.text("Wind Speed: " + response.wind.speed + " mph")
 
 
         ul.append(liTemp, liHumidity, liWind)
@@ -70,7 +98,7 @@ function weatherDisplay (event) {
         $(".cardTextToday").empty()
         $(".cardTextToday").append(ul)
 
-        var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${key}`
+        var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${key}&units=imperial`
         return $.ajax({
             url: requestUrl,
             method: 'GET'
@@ -78,8 +106,39 @@ function weatherDisplay (event) {
     }) .then (function (response){
         console.log(response);
         // code to display the 5 day forecast
-    }) 
+        var fiveDayData = response.list;
+        var weatherArray = [];
 
+        // object to store weather data from API
+        $.each(fiveDayData, function (index, value){
+            dataObject = {
+                date: value.dt_txt.split(' ')[0],
+                time: value.dt_txt.split(' ')[1],
+                temp: value.main.temp,
+                humidity: value.main.humidity,
+                wind: value.wind.speed,
+                icon: value.weather[0].icon
+            }
+            // push data at time 12 into my array
+            if (value.dt_txt(' ')[1] === "12:00:00"){
+                weatherArray.push(dataObject);
+            }
+        })
+        //display the 5 day forcast to the browser
+        for (let i = 0; i < myWeather.length; i++){
+            // create card for display of weather inside of the 5 day forecast element
+            var cardDiv = $("<div>");
+            cardDiv.attr("class", "card text-black bg-primary mb-3")
+            cardDiv.attr("style", "max-width: 200px;");
+
+            // add divs for elements inside of the card
+
+            var cardDivHead = $("<div>");
+            cardDivHead.attr()
+        } 
+    }) 
+    
+    // console.log(response.list) 
 }
 
 
